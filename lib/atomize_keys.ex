fc2a3dc6634590f04a3cb30c47b59f175d55e381
Uk Chukundah <ukchukundah@gmail.com>
2022-05-31 11:27:19 +0200

    Stringify atom keys

--- lib/atomize_keys.ex
+++ lib/atomize_keys.ex
@@ -18,9 +18,43 @@ defmodule AtomizeKeys do
 
   def atomize_string_keys(not_a_map), do: not_a_map
 
+  @doc """
+  Change atom map keys to strings.
+
+  ## Examples
+
+      iex> AtomizeKeys.stringify_atom_keys(%{a: 1, b: 2})
+      %{"a" => 1, "b" => 2}
+
+  """
+  def stringify_atom_keys(%_{} = term), do: term
+
+  def stringify_atom_keys(%{} = term), do: stringify_keys(term)
+
+  def stringify_atom_keys(term) when is_list(term), do: Enum.map(term, &stringify_keys/1)
+
+  def stringify_atom_keys(term), do: term
+
+  defp stringify_keys(nil), do: nil
+  defp stringify_keys(%_{} = struct), do: struct
+
+  defp stringify_keys(%{} = map) do
+    map
+    |> Enum.map(fn
+      {k, v} when is_atom(k) -> {to_string(k), stringify_keys(v)}
+      {k, v} -> {k, stringify_keys(v)}
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp stringify_keys([head | rest]), do: [stringify_keys(head) | stringify_keys(rest)]
+  defp stringify_keys(not_a_map), do: not_a_map
+
   defp atomize_keys(nil), do: nil
-  defp atomize_keys(struct = %{__struct__: _}), do: struct # Structs don't do enumerable and the keys are already atoms
-  defp atomize_keys(map = %{}) do
+  # Structs don't do enumerable and the keys are already atoms
+  defp atomize_keys(%_{} = struct), do: struct
+
+  defp atomize_keys(%{} = map) do
     map
     |> Enum.map(fn
       {k, v} when is_binary(k) -> {String.to_atom(k), atomize_keys(v)}
@@ -28,6 +62,7 @@ defmodule AtomizeKeys do
     end)
     |> Enum.into(%{})
   end
+
   defp atomize_keys([head | rest]), do: [atomize_keys(head) | atomize_keys(rest)]
   defp atomize_keys(not_a_map), do: not_a_map
 end
--- test/atomize_keys_test.exs
+++ test/atomize_keys_test.exs
@@ -7,22 +7,45 @@ defmodule AtomizeKeysTest do
   @list [:a, :b, :c]
   @number 4.5
 
-  test "atomizes string keys" do
-    atomized_map = AtomizeKeys.atomize_string_keys(@map)
-    assert Map.has_key?(atomized_map, :key)
-    refute Map.has_key?(atomized_map, "key")
+  describe "converting string keys" do
+    test "converts string keys to atoms" do
+      atomized_map = AtomizeKeys.atomize_string_keys(@map)
+      assert Map.has_key?(atomized_map, :key)
+      refute Map.has_key?(atomized_map, "key")
+    end
+
+    test "leaves non-string keys unchanged" do
+      atomized_map = AtomizeKeys.atomize_string_keys(@map)
+      assert Map.has_key?(atomized_map, [1,2])
+      assert Map.has_key?(atomized_map, 2)
+      assert Map.has_key?(atomized_map, :another_key)
+    end
+
+    test "only works on maps" do
+      assert "unchanged string" = AtomizeKeys.atomize_string_keys(@string)
+      assert [:a, :b, :c] = AtomizeKeys.atomize_string_keys(@list)
+      assert 4.5 = AtomizeKeys.atomize_string_keys(@number)
+    end
   end
 
-  test "leaves non-string keys unchanged" do
-    atomized_map = AtomizeKeys.atomize_string_keys(@map)
-    assert Map.has_key?(atomized_map, [1,2])
-    assert Map.has_key?(atomized_map, 2)
-    assert Map.has_key?(atomized_map, :another_key)
-  end
+  describe "converting atom keys" do
+    test "converts atom keys to strings" do
+      stringified_map = AtomizeKeys.stringify_atom_keys(@map)
+      assert Map.has_key?(stringified_map, "another_key")
+      refute Map.has_key?(stringified_map, :key)
+    end
 
-  test "only works on maps" do
-    assert "unchanged string" = AtomizeKeys.atomize_string_keys(@string)
-    assert [:a, :b, :c] = AtomizeKeys.atomize_string_keys(@list)
-    assert 4.5 = AtomizeKeys.atomize_string_keys(@number)
+    test "leaves non-atom keys unchanged" do
+      stringified_map = AtomizeKeys.stringify_atom_keys(@map)
+      assert Map.has_key?(stringified_map, [1,2])
+      assert Map.has_key?(stringified_map, 2)
+      assert Map.has_key?(stringified_map, "key")
+    end
+
+    test "only works on maps" do
+      assert "unchanged string" = AtomizeKeys.stringify_atom_keys(@string)
+      assert [:a, :b, :c] = AtomizeKeys.stringify_atom_keys(@list)
+      assert 4.5 = AtomizeKeys.stringify_atom_keys(@number)
+    end
   end
 end
